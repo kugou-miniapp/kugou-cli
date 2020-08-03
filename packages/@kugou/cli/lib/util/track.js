c9d68215d9d33c59282ff733d883b34c178b9f5b
梁嘉辉 <liangjiahui@liangjiahuideMacBook-Pro.local>
2020-08-03 23:14:15 +0800

    fix(cli): 版本

--- packages/@kugou/cli/lib/util/track.js
+++ packages/@kugou/cli/lib/util/track.js
@@ -6,10 +6,10 @@ const mid = getMac()
 const tracker = new Tracker()
 
 module.exports = function(data) {
-  tracker.track({
-    b: '统计',
-    r: '小程序',
-    mid,
-    ...data
-  })
+  // tracker.track({
+  //   b: '统计',
+  //   r: '小程序',
+  //   mid,
+  //   ...data
+  // })
 }
